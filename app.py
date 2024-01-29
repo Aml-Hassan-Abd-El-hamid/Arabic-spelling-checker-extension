2bec3673baff6f68159db6e9f2d21df7a246020e
Aml <aml.hassan.esmil@gmail.com>
2024-01-29 18:50:39 +0200

    add api in its simplest form

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 from ar_corrector.corrector import Corrector
-corr = Corrector()
+from flask import Flask as f, jsonify,request
+#from flask import render_template
 
-print(corr.spell_correct('بختب'))
+corr = Corrector()
+app = f(__name__)
+
+def call_corr(word):
+    return corr.spell_correct(word)
+
+@app.route('/')
+def home():
+    #return render_template('home.html')
+    #return jsonify(message='hi')
+    word= request.args.get("word")
+    return jsonify(words=call_corr(word))
+
+if __name__ == '__main__':
+    app.run()
